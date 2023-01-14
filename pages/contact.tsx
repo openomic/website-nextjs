a99f0a584ff86d734b249dae78100989cba3cb1e
Rasmus Buchholdt <rasmus.buchholdt@gmail.com>
2023-01-14 10:54:11 +0100

    Better contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -4,46 +4,49 @@ export default function Contact() {
   return (
     <Layout>
       <section className="section-box">
-        <div className="banner-hero banner-breadcrums">
-          <div className="container text-center">
-            <h1 className="text-heading-2 color-gray-1000 mb-20">Contact Us</h1>
-            <p className="text-body-text color-gray-500">
-              Equidem necessitatibus ei eam, ceteros expetenda
-              <br className="d-lg-block d-none" />
-              hendrerit ei per, tation vituperatoribus ut.
-            </p>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12 col-sm-10 col-12 text-center mt-40">
+              <h2 className="text-heading-1 color-gray-900 mb-20">
+                Kontakt os
+              </h2>
+            </div>
           </div>
         </div>
       </section>
       <section className="section-box">
-        <div className="container mb-20 mt-140">
+        <div className="container mb-20 mt-50">
           <div className="bdrd-58 box-gray-100 icon-wave">
             <div className="row">
               <div className="col-lg-12 mb-60">
                 <span className="text-body-capitalized text-uppercase">
-                  Contact us
+                  Kontakt os
                 </span>
                 <h2 className="text-heading-3 color-gray-900 mt-10">
-                  Have an prject in mind?
+                  Udfyld formularen så vi kan tage kontakt til dig
                 </h2>
                 <p className="text-body-text color-gray-600 mt-20">
-                  The right move at the right time saves your investment.
-                  <br className="d-lg-block d-none" />
-                  live the dream of expanding your business.
+                  Hvis du har et projekt eller et generelt spørgsmål, kan du
+                  kontakte os her - Vi bestræber os på at svare indenfor to
+                  hverdage.
+                </p>
+                <p className="text-body-text color-gray-600 mt-10">
+                  Alle henvendelser er helt uforpligtende.
                 </p>
               </div>
               <div className="col-lg-4 mb-40">
                 <h4 className="text-heading-6 color-gray-900 icon-home mb-10 mt-10">
-                  Agon Studio
+                  Openomic ApS
                 </h4>
                 <p className="text-body-text color-gray-600">
-                  4517 Washington Ave.
-                  <br />
-                  Manchester, Kentucky 39495
+                  xxxxxxxxx
                 </p>
-                <p className="text-body-text color-gray-600">(239) 555-0108</p>
                 <p className="text-body-text color-gray-600">
-                  contact@agon.com
+                  xxxxxxxxx
+                </p>
+                <p className="text-body-text color-gray-600">12345678</p>
+                <p className="text-body-text color-gray-600">
+                  kontakt@openomic.dk
                 </p>
               </div>
               <div className="col-lg-8">
