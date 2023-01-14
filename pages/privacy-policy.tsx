64a32ec8806ad88761fce806f364b272c2ef1744
Rasmus Buchholdt <rasmus.buchholdt@gmail.com>
2023-01-14 12:11:19 +0100

    Placeholder pages

---
+++ pages/privacy-policy.tsx
@@ -0,0 +1,31 @@
+import { Layout } from '../components/layout/Layout';
+
+export default function About() {
+  return (
+    <Layout>
+      <section className="section-box pt-50 pb-50">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12 mb-40">
+              <h3 className="text-heading-1">Privatlivspolitik</h3>
+              <p className="text-body-text color-gray-600 mt-10">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+                a nisi sit amet lectus sodales pellentesque vel id tellus. Nam
+                viverra porttitor mi at vestibulum. Etiam ut ex in mauris
+                lacinia placerat. Duis ornare pretium orci ac dapibus. In
+                commodo non ante vitae varius. Cras sit amet ornare sapien.
+              </p>
+              <p className="text-body-text color-gray-600 mt-10">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+                a nisi sit amet lectus sodales pellentesque vel id tellus. Nam
+                viverra porttitor mi at vestibulum. Etiam ut ex in mauris
+                lacinia placerat. Duis ornare pretium orci ac dapibus. In
+                commodo non ante vitae varius. Cras sit amet ornare sapien.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </Layout>
+  );
+}
