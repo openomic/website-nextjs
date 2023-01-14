514c51e4b87ae741b64c117d703b28b0ea1231ac
Rasmus Buchholdt <rasmus.buchholdt@gmail.com>
2023-01-14 10:19:32 +0100

    Port over about us text

--- pages/about.tsx
+++ pages/about.tsx
@@ -11,25 +11,54 @@ export async function getServerSideProps() {
   return { props: { employees } };
 }
 
-export default function AboutUs({
+export default function About({
   employees,
 }: {
   employees: OpReturnType<operations["get/employees"]>;
 }) {
   return (
     <Layout>
-      <section className="section-box">
-        <div className="container mt-110">
+      <section className="section-box pt-50 pb-50">
+        <div className="container">
           <div className="row">
-            <div className="col-lg-9 col-sm-8">
-              <h3 className="text-heading-1 mb-10">Our Team</h3>
-              <p className="text-body-lead-large color-gray-600">
-                Decades of experience in design and development
+            <div className="col-lg-12 mb-40">
+              <h3 className="text-heading-1">Vores mission</h3>
+              <p className="text-body-lead-large color-gray-600 mt-10">
+                Hos Openomic kan vi løse et hav af forskellige opgavetyper, med
+                den erfaring vi har opnået gennem vores tilsammen 15+ års
+                erfaring. Vi kan hjælpe dig uanset om du selv har erfaring med
+                udvikling eller ej.
+              </p>
+              <p className="text-body-text color-gray-600 mt-10">
+                Vi kan udvikle og levere skræddersyede softwareløsninger såvel
+                som stå for hosting af det færdige produkt. Vi er med dig hele
+                vejen fra start til slut - også efter produktet er udviklet.
+              </p>
+              <p className="text-body-text color-gray-600 mt-10">
+                Hvis du allerede har en software- eller webløsning, der mangler
+                nye features eller vedligeholdelse kan vi også være behjælpelige
+                med dette i mange tilfælde, da vi har erfaring med en bred vifte
+                af forskellige teknologier.
+              </p>
+              <p className="text-body-text color-gray-600 mt-10">
+                Kontakt os endeligt for et uforpligtende tilbud.
               </p>
             </div>
           </div>
         </div>
-        <div className="container mt-80">
+      </section>
+      <section className="section-box">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12 col-sm-8">
+              <h3 className="text-heading-1 mb-10">Mød holdet</h3>
+              <p className="text-body-lead-large color-gray-600">
+                Folket bag Openomic
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="container mt-50">
           <div className="box-swiper">
             <div className="swiper-container swiper-group-1">
               {employees.data?.map((employee) => (
@@ -52,9 +81,6 @@ export default function AboutUs({
                         <p className="text-body-excerpt text-desc color-gray-500 mt-15 mb-20">
                           {employee.attributes?.description}
                         </p>
-                        <span className="text-body-text-md color-gray-600">
-                          Let’s Connect
-                        </span>
                         <div className="social-bottom">
                           <Link href="#" legacyBehavior>
                             <a className="icon-socials icon-facebook"></a>
